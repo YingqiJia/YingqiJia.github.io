7d7996073eaec3651544e94ce13affc594e7651b
yingqijia <yingqi098@gmail.com>
2024-01-16 23:04:22 +0200

    Init Unity WebGL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
+function App() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "YingqiJia.github.io/build/museum.loader.js", // public下目录
+    dataUrl: "YingqiJia.github.io/build/museum.data",
+    frameworkUrl: "YingqiJia.github.io/build/museum.framework.js",
+    codeUrl: "YingqiJia.github.io/build/museum.wasm",
+  });
+  // 一定要给Unity组件设置width和height属性，否则Canvas将无限增大最终导致浏览器卡死
+  return (
+    <Unity unityProvider={unityProvider} style={{ width: 960, height: 600 }}/>
+  );
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -20,6 +36,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
